98140313ed64adc961d14570f1aa7bb03ad1b214
Joshua Hedstrom <jhhedstrom@gmail.com>
2021-09-30 14:38:43 -0400

    commit tree f74312f3f24053863429bf1c22a1008f31222a98

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, string){
+   return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(names, string1){
+    let lengthOfName = string1.length;
+    return names.filter(function (driverName){
+        return driverName.slice(0, lengthOfName) === string1;
+    });
+}    
+
+function matchName (obj, name) {
+    return obj.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
